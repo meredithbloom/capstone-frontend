5b99154326c31cbebbb5ac06e90d194da7e6632c
Scarlet Puma <scarlt.puma@gmail.com>
2022-03-23 11:50:55 -0400

    sass files and variables

--- capstone_frontend/src/App.js
+++ capstone_frontend/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import './styles/home.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <button>x</button>
+    </>
+  )
 }
-
-export default App;
+export default App
